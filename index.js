83632e0727c39ab576d43fec7b1ec07bc890a397
joao gabriel <joao.gabriel.msilva10@gmail.com>
2026-09-02 21:04:49 -0300

    quase  rodando

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 import express from 'express'
 import { listen } from 'node:quic';
 
-const app = espress()
+const app = express()
 
-app.use(espress.json);
+app.use(express.json);
 
 app.get("/api/matematica/:num2/:num2", (req, res) => {
-    const num1 = Number(res.params.num1)
-    const num2 = Number(res.params.num2)
-
-    res.send({ menssage: num1 - num2 })
+    
+    res.send({ menssage: "Operaçoes: Subtraçao, soma, multiplicaçao, divisao, raiz e potencia" });
 })
 
-app.listen(3300 () =
+app.post("/api/opracoes/num1/num2", (req, res) => {
+    const num1 = res.body.num1
+    const num2 = res.body.num2
+
+    const resp1 = num1 - num2
+    const resp2 = num1 + num2
+    const resp3 = num1 * num2
+    const resp4 = num1 / num2
+    const resp5 = num1^2
+    
+
+    res.send({ menssage: resp1,
+        menssage: resp2,
+        menssage: resp3,
+        menssage: resp4
+    })
+})
+
+app.listen(3000, () => { 
   console.log("Rodando na porta localhost3000")
-)
+});
